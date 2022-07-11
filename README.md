f6e407a37e09ba373012d4b01a27740f1a91cecf
ian <ian@not-gaming>
2022-07-10 23:21:28 -0400

    updates are in readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # troll-bot
 A discrod bot written in python that has multable functons.
 [You can add the bot here.](https://discord.com/oauth2/authorize?client_id=702906230703783957&permissions=8&scope=bot)
+<<<<<<< HEAD
+# update sunday July 10 2022
+    Added more in the things for the troll function.
+    Redid the help function.
+    And the man behind the sluather is here.
+    Send help, can't spell lol.
+=======
+>>>>>>> 42d40fb (Readme)
--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#GPL 3.0
 import discord
 from discord.ext import commands
 import time
@@ -27,7 +26,22 @@ ls = [
         "https://tenor.com/view/funny-purchase-college-barack-obama-obama-barack-obama-gif-19072900", 
         "https://tenor.com/view/rat-make-out-rat-sniff-hey-do-you-want-to-make-out-do-you-want-to-make-out-gif-23560859",
         "https://tenor.com/view/turtle-animal-dance-cute-gif-8300057",
-        "https://tenor.com/view/clash-royale-clash-royale-emote-gif-23858585", # 0 - 19?
+        "https://tenor.com/view/clash-royale-clash-royale-emote-gif-23858585",
+        "https://tenor.com/view/cry-about-it-space-engineers-gif-24697313O",
+        "https://tenor.com/view/touch-grass-touch-grass-gif-21734295",
+        "https://tenor.com/view/geico-geico-commercial-geico-wood-chuck-wood-chuck-chucking-gif-16412432"
+        "https://c.tenor.com/kwJ4fEC7W4IAAAAM/pvz-plants.gif",
+        "https://tenor.com/view/fail-lawn-mower-dragged-gif-7931983",
+        "https://tenor.com/view/no-yes-gif-23115256",
+        "https://tenor.com/view/get-off-my-lawn-old-lady-sprinkler-grumpy-angry-gif-19033636",
+        "https://tenor.com/view/pvz-plants-vs-zombies-plant-vs-zombie-plant-zombie-pea-gif-19750806",
+        "https://c.tenor.com/WwEyrLnTpxQAAAAM/didnt-ask-didnt.gif",
+        "https://media.discordapp.net/attachments/895418619666784286/928808807046451240/FE4A1361-C273-4597-BC48-D3431A35E002.gif",
+        "https://tenor.com/view/troll-trollface-scary-void-scary-troll-face-gif-19777211",
+        "https://www.youtube.com/watch?v=-imBnKqgKQs",
+        "https://youtu.be/9eC_dmVemaw",
+        "https://c.tenor.com/oSIiS53J_UMAAAAM/cat-dancing.gif",
+    # 0 - 27???????? lol
 ]
 iv = [
         "https://tenor.com/view/civ-civ6-get-on-civ-gif-22961237",
@@ -36,6 +50,11 @@ iv = [
         "https://tenor.com/view/gandhi-india-ftw-victory-winning-gif-8758824",
         "https://tenor.com/view/civ-civilization-gilgamesh-gilgabro-fan-gif-21095212",
         ]
+TMBTS = [
+        "https://tenor.com/view/purple-guy-purple-guy-fnaf-apollothealmighty-fnaf-fnaf-security-breach-gif-2485131",
+        "https://tenor.com/view/purple-guy-murder-roblox-man-behind-slaughter-mm2-gif-17490514",
+        "https://tenor.com/view/purple-guy-gif-18501497",
+    ]
 @bot.command()
 async def pancake_mix(ctx):
     await ctx.send("https://www.amazon.com/Crocker-Bisquick-Complete-Pancake-Buttermilk/dp/B00DJQ2B2W/ref=zg_bs_16317311_22/144-2364321-1683057?pd_rd_i=B00DJQ2B2W&psc=1")
@@ -45,6 +64,10 @@ async def troll(ctx):
     x = random.choice(ls)
     await ctx.send(x)
 
+@bot.command()
+async def purpleguy(ctx):
+    x = random.choice(TMBTS)
+    await ctx.send(x)
 
 @bot.event
 async def on_ready():
@@ -54,11 +77,13 @@ async def on_ready():
 @commands.has_permissions(administrator=True)
 async def trollspam(ctx,num,*,msg):
     x = 1
+    if 500 < int(num):
+        num = '500'
     while x <= int(num):
         await ctx.send(msg)
         print( x ,'/' , num , ',' , msg)
         time.sleep(0.75)
-        x = x + 1
+        x += 1
 
 
 
@@ -71,7 +96,7 @@ async def civ(ctx):
 async def ip(ctx,user):
 
     await ctx.channel.purge(limit=1)
-    ip = (str(random.randint(0 , 999)) + '.' + str(random.randint(0 , 999)) + '.' + str(random.randint(0 , 999)) + '.' + str(random.randint(0 , 999)))
+    ip = (str(random.randint(0 , 255)) + '.' + str(random.randint(0 , 255)) + '.' + str(random.randint(0 , 255)) + '.' + str(random.randint(0 , 255)))
     await ctx.send(f'{user} your ip is {ip} your opinion is now null.')
     print(f'{user} your ip is {ip} your opinion is now null.')
     
@@ -82,7 +107,9 @@ async def trollclear(ctx, num=2):
 
 @bot.command()
 async def help(ctx):
-    await ctx.send('There is no help. There is no hope.')
+    ip = (str(random.randint(0 , 255)) + '.' + str(random.randint(0 , 255)) + '.' + str(random.randint(0 , 255)) + '.' + str(random.randint(0 , 255)))
+    await ctx.send(f'look familiar {ip}    :^).')
+    await ctx.send('https://tenor.com/view/freddy-freddy-fazbear-freddy-fnaf-fnaf-fnaf-meme-gif-24466510')
 
 @bot.command()
 async def swiss_roll(ctx):
@@ -90,4 +117,4 @@ async def swiss_roll(ctx):
 
 
 
-bot.run('[Privite token here]')
+bot.run('Token Goes Here')
